9dc490d20fdd481fa78fb022180923351f62a9a8
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-07-24 15:10:16 -0700

    Footer work

--- app.config.js
+++ app.config.js
@@ -39,12 +39,10 @@ const appConfig = {
    * @type {[key: 'twitterUrl' | 'facebookUrl' | 'instagramUrl' | 'youtubeUrl' | 'githubUrl' | 'linkedinUrl']: string}
    */
   socialLinks: {
-    twitterUrl: 'https://twitter.com/wpengine',
-    facebookUrl: 'https://www.facebook.com/wpengine',
-    instagramUrl: 'https://www.instagram.com/wpengine/',
-    youtubeUrl: 'https://youtube.com/wpengine',
-    githubUrl: 'https://github.com/wpengine',
-    linkedinUrl: 'https://www.linkedin.com/company/wpengine',
+    twitterUrl: 'https://x.com/CalPolyPartners',
+    facebookUrl: 'https://www.facebook.com/calpolypartners/',
+    instagramUrl: 'https://www.instagram.com/calpoly_partners/',
+    linkedinUrl: 'https://www.linkedin.com/company/calpolypartners',
   },
 };
 
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,12 +1,12 @@
 import classNames from 'classnames/bind';
 import {
   FaFacebookF,
-  FaGithub,
   FaInstagram,
   FaLinkedinIn,
-  FaTwitter,
-  FaYoutube,
-} from 'react-icons/fa';
+  FaXTwitter,
+} from 'react-icons/fa6';
+import Image from 'next/image';
+import Link from 'next/link';
 import appConfig from 'app.config.js';
 
 import { NavigationMenu } from '../';
@@ -19,109 +19,159 @@ let cx = classNames.bind(styles);
  * The Blueprint's Footer component
  * @return {React.ReactElement} The Footer component.
  */
-export default function Footer({ title, menuItems }) {
+export default function Footer({
+  siteTitle,
+  title,
+  menuItems, // PRIMARY footer menu
+  navOneMenuItems, // SECONDARY footer menu
+  navTwoMenuItems, // TERTIARY footer menu
+}) {
   return (
     <footer className={cx('footer')}>
       <div className="container">
-        {appConfig?.socialLinks && (
-          <div className={cx('social-links')}>
+
+        <div className={cx('footer-nav-contact-info')}>
+          <div className={cx('footer-nav')}>
+            <h3>Quick Links</h3>
+            <NavigationMenu className={cx('quick')} menuItems={menuItems} />
+          </div>
+
+          <div className={cx('contact-info')}>
+            <a href="/" className={cx('cppText')}>
+              {title ?? 'Cal Poly Partners'}
+            </a>
+            <a href="tel:8057561451" className={cx('phone')}>
+              (805) 756-1451
+            </a>
+
+            {appConfig?.socialLinks && (
+              <div className={cx('social-links')}>
+                <ul aria-label="Social media">
+                  {appConfig.socialLinks?.facebookUrl && (
+                    <li>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={cx('social-icon-link')}
+                        href={appConfig.socialLinks.facebookUrl}
+                      >
+                        <FaFacebookF className={cx('social-icon')} />
+                      </a>
+                    </li>
+                  )}
+                  {appConfig.socialLinks?.instagramUrl && (
+                    <li>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={cx('social-icon-link')}
+                        href={appConfig.socialLinks.instagramUrl}
+                      >
+                        <FaInstagram className={cx('social-icon')} />
+                      </a>
+                    </li>
+                  )}
+                  {appConfig.socialLinks?.linkedinUrl && (
+                    <li>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={cx('social-icon-link')}
+                        href={appConfig.socialLinks.linkedinUrl}
+                      >
+                        <FaLinkedinIn className={cx('social-icon')} />
+                      </a>
+                    </li>
+                  )}
+                  {appConfig.socialLinks?.twitterUrl && (
+                    <li>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={cx('social-icon-link')}
+                        href={appConfig.socialLinks.twitterUrl}
+                      >
+                        <FaXTwitter className={cx('social-icon')} />
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className={cx('logo-address')}>
+          <div className={cx('logo')}>
+            <Link legacyBehavior href="/">
+              <a title="Home">
+                <Image
+                  src="/logo.png"
+                  width={400}
+                  height={80}
+                  alt="Cal Poly University logo"
+                  layout="responsive"
+                />
+              </a>
+            </Link>
+          </div>
+
+          <p>1 Grand Avenue, San Luis Obispo, CA 93407</p>
+
+          <a href="tel:8057561111" className={cx('phone')}>
+            (805) 756-1111
+          </a>
+
+          <div className={cx('social-links', 'cp-links')}>
             <ul aria-label="Social media">
-              {appConfig.socialLinks?.twitterUrl && (
-                <li>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('social-icon-link')}
-                    href={appConfig.socialLinks.twitterUrl}
-                  >
-                    <FaTwitter title="Twitter" className={cx('social-icon')} />
-                  </a>
-                </li>
-              )}
-
-              {appConfig.socialLinks?.facebookUrl && (
-                <li>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('social-icon-link')}
-                    href={appConfig.socialLinks.facebookUrl}
-                  >
-                    <FaFacebookF
-                      title="Facebook"
-                      className={cx('social-icon')}
-                    />
-                  </a>
-                </li>
-              )}
-
-              {appConfig.socialLinks?.instagramUrl && (
-                <li>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('social-icon-link')}
-                    href={appConfig.socialLinks.instagramUrl}
-                  >
-                    <FaInstagram
-                      title="Instagram"
-                      className={cx('social-icon')}
-                    />
-                  </a>
-                </li>
-              )}
-
-              {appConfig.socialLinks?.youtubeUrl && (
-                <li>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('social-icon-link')}
-                    href={appConfig.socialLinks.youtubeUrl}
-                  >
-                    <FaYoutube title="YouTube" className={cx('social-icon')} />
-                  </a>
-                </li>
-              )}
-
-              {appConfig.socialLinks?.githubUrl && (
-                <li>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('social-icon-link')}
-                    href={appConfig.socialLinks.githubUrl}
-                  >
-                    <FaGithub title="GitHub" className={cx('social-icon')} />
-                  </a>
-                </li>
-              )}
-
-              {appConfig.socialLinks?.linkedinUrl && (
-                <li>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cx('social-icon-link')}
-                    href={appConfig.socialLinks.linkedinUrl}
-                  >
-                    <FaLinkedinIn
-                      title="LinkedIn"
-                      className={cx('social-icon')}
-                    />
-                  </a>
-                </li>
-              )}
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={cx('social-icon-link')}
+                  href="https://www.facebook.com/CalPoly/"
+                >
+                  <FaFacebookF className={cx('social-icon')} />
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={cx('social-icon-link')}
+                  href="https://www.instagram.com/calpoly/"
+                >
+                  <FaInstagram className={cx('social-icon')} />
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={cx('social-icon-link')}
+                  href="https://x.com/CalPoly"
+                >
+                  <FaXTwitter className={cx('social-icon')} />
+                </a>
+              </li>
             </ul>
           </div>
-        )}
+        </div>
 
-        <NavigationMenu className={cx('nav')} menuItems={menuItems} />
+        {/* 🔥 These are your custom menus */}
+        <div className={cx('nav-one')}>
+          <NavigationMenu className={cx('nav')} menuItems={navOneMenuItems} />
+        </div>
+
+        <div className={cx('nav-two')}>
+          <NavigationMenu className={cx('nav')} menuItems={navTwoMenuItems} />
+        </div>
 
         <div className={cx('copyright')}>
-          &copy; {new Date().getFullYear()} {title}
+          &copy; {new Date().getFullYear()} {siteTitle ?? 'Cal Poly Partners'}
         </div>
       </div>
+      {console.log(menuItems, navOneMenuItems, navTwoMenuItems)}
     </footer>
   );
 }
