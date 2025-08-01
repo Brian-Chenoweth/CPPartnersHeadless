fe2fc2ae5cbfe9966959570ec249bcc4c8ec61b3
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-07-31 18:00:16 -0700

    Ticker work

--- components/ContentWrapper/ContentWrapper.js
+++ components/ContentWrapper/ContentWrapper.js
@@ -1,21 +1,53 @@
+import parse, { domToReact } from 'html-react-parser';
 import className from 'classnames/bind';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import styles from './ContentWrapper.module.scss';
 
 const cx = className.bind(styles);
 
-/**
- * A basic Container Wrapper component
- * @param {Props} props The props object.
- * @param {string} props.content The content as string.
- * @param {string} props.className An optional className to be added to the container.
- * @param {React.ReactElement} props.children The children to be rendered.
- * @return {React.ReactElement} The ContentWrapper component.
- */
 export default function ContentWrapper({ content, className, children }) {
+  const transform = (node) => {
+    const isTickerGroup =
+      node.name === 'div' &&
+      node.attribs?.class?.includes('wp-block-group') &&
+      node.attribs?.class?.includes('ticker');
+
+    if (isTickerGroup) {
+      const figures = node.children?.filter(
+        (child) =>
+          child.name === 'figure' &&
+          child.attribs?.class?.includes('wp-block-image')
+      );
+
+      return (
+        <Carousel
+          autoPlay
+          infiniteLoop={false}      // ❌ Don't loop back to the beginning
+          interval={3000}
+          showArrows={true}
+          showThumbs={false}
+          showStatus={false}
+          showIndicators={false}
+          stopOnHover={true}
+          centerMode={true}
+          centerSlidePercentage={33}
+          swipeable={true}
+          emulateTouch={true}
+          transitionTime={700}      // optional: slow down transition
+        >
+          {figures.map((fig, index) => (
+            <div key={index}>{domToReact([fig])}</div>
+          ))}
+        </Carousel>
+      );
+    }
+  };
+
   return (
     <article className={cx('content', className)}>
-      <div dangerouslySetInnerHTML={{ __html: content ?? '' }} />
+      {parse(content ?? '', { replace: transform })}
       {children}
     </article>
   );
