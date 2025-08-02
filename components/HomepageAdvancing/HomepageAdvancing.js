fb23a942dfcbfadf266cc905685ea2d46f9a2478
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-02 13:20:09 -0700

    Class to ClassName

--- components/HomepageAdvancing/HomepageAdvancing.js
+++ components/HomepageAdvancing/HomepageAdvancing.js
@@ -6,8 +6,8 @@ import styles from './HomepageAdvancing.module.scss';
 export default function HomepageAdvancing() {
   return (
     <div className={styles.cta}>
-      <div class="wp-block-group intro-text bg-green-dark has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
-        <h2 class="wp-block-heading">Advancing Learn by Doing
+      <div className="wp-block-group intro-text bg-green-dark has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
+        <h2 className="wp-block-heading">Advancing Learn by Doing
         </h2>
         <p>We are an active supporter of Cal Poly’s “Learn by Doing” philosophy. Cal Poly Partners provides fiscal and administrative services to commercial units that involve student staffing and learning. From Swanton Pacific Ranch to the Center for Innovation and Entrepreneurship, University Graphic Systems, and various agricultural enterprise operations, we facilitate hands-on learning experiences that prepare students for the real world.</p>
         <p>
