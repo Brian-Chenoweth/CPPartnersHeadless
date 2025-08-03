d5a84b1391367a09f80ec5837ddadc1621553919
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-08-02 17:12:23 -0700

    HomepageIntro component

--- components/HomepageIntro/HomepageIntro.js
+++ components/HomepageIntro/HomepageIntro.js
@@ -1,10 +1,42 @@
+import Link from 'next/link';
 import styles from './HomepageIntro.module.scss';
-
 
 export default function HomepageIntro() {
   return (
-    <div className={styles.cta}>
-      <div className={styles.content}><h3>HomepageIntro</h3></div>
-    </div>
+    <section className={`bg-green ${styles.homeIntro}`}>
+      <div>
+        <div className={styles.box}>
+          <div className={styles.heading}>
+            <h2>
+              As the university's nonprofit auxiliary, Cal Poly Partners' purpose is
+              crystal clear: to propel Cal Poly forward.
+            </h2>
+          </div>
+
+          <div className={styles.content}>
+            <p>
+              Cal Poly Partners is a mission-driven organization built on the belief of
+              positive doing to elevate education, empower individuals and enhance the
+              university experience.
+            </p>
+            <p>
+              Through our flexible approach and an unyielding spirit, we break boundaries
+              to provide essential university resources and services. Our unwavering
+              commitment to these principles is what sets us apart.
+            </p>
+            <p>
+              At Cal Poly Partners, we don’t just provide services; we forge partnerships
+              that shape the future. Join us in building a brighter tomorrow for Cal Poly
+              and its community. Together, we can accomplish more than we ever imagined.
+            </p>
+            <p>
+              <Link legacyBehavior href="/news/">
+                <a title="News">News</a>
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
