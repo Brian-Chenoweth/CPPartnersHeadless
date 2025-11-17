c1d460aac76a4601c796e2eb5ab1f586feb6cc10
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-11-17 15:23:29 -0800

    SEO

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -2,59 +2,32 @@ import Head from 'next/head';
 
 /**
  * Provide SEO related meta tags to a page.
+ *
+ * @param {Props} props The props object.
+ * @param {string} props.title Used for the page title, og:title, twitter:title, etc.
+ * @param {string} props.description Used for the meta description, og:description, twitter:description, etc.
+ * @param {string} props.imageUrl Used for the og:image and twitter:image.
+ * @param {string} props.url Used for the og:url and twitter:url.
+ *
+ * @returns {React.ReactElement} The SEO component
  */
 export default function SEO({ title, description, imageUrl, url }) {
   if (!title && !description && !imageUrl && !url) {
     return null;
   }
 
-  const typekitStylesheets = [
-    'https://use.typekit.net/umi1lem.css',
-    'https://use.typekit.net/mfv5sni.css',
-    'https://use.typekit.net/qnm1phw.css',
-    'https://use.typekit.net/ato6pec.css',
-  ];
-
   return (
     <>
       <Head>
-        {/* Preconnect for Adobe Typekit */}
-        <link rel="preconnect" href="https://use.typekit.net" />
-        <link rel="preconnect" href="https://p.typekit.net" crossOrigin="anonymous" />
-
-        {/* Load Typekit CSS asynchronously to avoid render blocking */}
-        {typekitStylesheets.map((href) => (
-          <link key={`${href}-preload`} rel="preload" href={href} as="style" />
-        ))}
-
-        {typekitStylesheets.map((href) => (
-          <link
-            key={href}
-            rel="stylesheet"
-            href={href}
-            media="print"
-            onLoad={(e) => {
-              e.currentTarget.media = 'all';
-            }}
-          />
-        ))}
-
-        {/* Fallback for no-JS users */}
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: typekitStylesheets
-              .map((href) => `<link rel="stylesheet" href="${href}">`)
-              .join('\n'),
-          }}
-        />
-
-        {/* Google Fonts (already display=swap, fine to keep) */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="stylesheet" href="https://use.typekit.net/umi1lem.css"/>
+        <link rel="stylesheet" href="https://use.typekit.net/mfv5sni.css"/>
+        <link rel="stylesheet" href="https://use.typekit.net/qnm1phw.css"/>
+        <link rel="stylesheet" href="https://use.typekit.net/ato6pec.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"/>
+        {/* <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet"/> */}
+
 
         <meta property="og:type" content="website" />
         <meta property="twitter:card" content="summary_large_image" />
