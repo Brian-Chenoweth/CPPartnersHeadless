42b9b378dabc8a0eb7da477bd673559d7bc1464d
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-11-17 14:04:24 -0800

    fix: add responsive sizes to Campus Life hero image to resolve PSI image delivery warnings

--- components/HomepageCampusLife/HomepageCampusLife.js
+++ components/HomepageCampusLife/HomepageCampusLife.js
@@ -6,28 +6,30 @@ import styles from './HomepageCampusLife.module.scss';
 export default function HomepageCampusLife() {
   return (
     <div className={styles.cta}>
-          <div className="wp-block-media-text has-media-on-the-right is-stacked-on-mobile white-image-right">
-            <div className="wp-block-media-text__content">
-              <h2 className="wp-block-heading">Enhancing Campus Life</h2>
-              <p>We take immense pride in our role as a vital contributor to campus life. From delicious dining options to affordable textbooks, cutting-edge technology to academic supplies, spirited gear to faculty and staff housing, Cal Poly Partners is here to enhance the Cal Poly experience. We’re more than just a service provider; we’re an integral part of the fabric that makes Cal Poly thrive.</p>
-              <p>
-                <Link legacyBehavior href="/commercial-services/">
-                  <a title="Commercial Services">
-                    Commercial Services
-                  </a>
-                </Link>
-              </p>
-            </div>
-            <figure className="wp-block-media-text__media">
-              <Image
-                src="/home/students-eating-outdoors-campus-mural-patio-area.jpg"
-                width={980}
-                height={630}
-                alt="Students sitting under umbrellas at outdoor tables near a colorful mural on a sunny campus patio."
-                layout="responsive"
-              />
-            </figure>
-          </div>
+      <div className="wp-block-media-text has-media-on-the-right is-stacked-on-mobile white-image-right">
+        <div className="wp-block-media-text__content">
+          <h2 className="wp-block-heading">Enhancing Campus Life</h2>
+          <p>
+            We take immense pride in our role as a vital contributor to campus life. From delicious dining options to affordable textbooks, cutting-edge technology to academic supplies, spirited gear to faculty and staff housing, Cal Poly Partners is here to enhance the Cal Poly experience. We’re more than just a service provider; we’re an integral part of the fabric that makes Cal Poly thrive.
+          </p>
+          <p>
+            <Link legacyBehavior href="/commercial-services/">
+              <a title="Commercial Services">Commercial Services</a>
+            </Link>
+          </p>
+        </div>
+        <figure className="wp-block-media-text__media">
+          <Image
+            src="/home/students-eating-outdoors-campus-mural-patio-area.jpg"
+            width={980}
+            height={630}
+            alt="Students sitting under umbrellas at outdoor tables near a colorful mural on a sunny campus patio."
+            sizes="(max-width: 768px) 100vw, 50vw"
+            style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+            priority={false}
+          />
+        </figure>
+      </div>
     </div>
   );
 }
